f611b831e2fff95f780572bfebab39ca5de74ac1
ik17 <ikubat1@etf.unsa.ba>
2019-09-07 14:30:20 +0200

    Added labels to the graphs.
    Co-Authored-By: hannabojadzic <hannabojadzic@users.noreply.github.com>

--- bssProject.m
+++ bssProject.m
@@ -109,6 +109,8 @@ avgSecondHigh = mean(secondHigh)
 avgFirstLow = mean(firstLow)
 avgSecondLow = mean(secondLow)
 avg = mean(age);
+humidity1 = CSVData(1, 5);
+humidity2 = CSVData(1, 6);
 set(handles.text5, 'String', firstRow(1));
 set(handles.text6, 'String', firstRow(3));
 set(handles.text9, 'String', firstRow(2));
@@ -116,6 +118,8 @@ set(handles.text10, 'String', firstRow(4));
 set(handles.text15, 'String', strcat(strcat(num2str(avgFirstHigh),'/'),num2str(avgFirstLow)));
 set(handles.text16, 'String', strcat(strcat(num2str(avgSecondHigh),'/'),num2str(avgSecondLow)));
 set(handles.text19, 'String', num2str(avg));
+set(handles.text24, 'String', strcat(num2str(humidity1), '%'));
+set(handles.text25, 'String', strcat(num2str(humidity2), '%'));
 
 %set(handles.editMaxRPM, 'Double', maxRPM);
 % hObject    handle to Open (see GCBO)
@@ -133,11 +137,17 @@ B= unique(A);
 subplot(2, 2, 3);
 C = categorical(A,B,cellstr(num2str(B)));
 histogram(C);
+title('Razlike u gornjem pritisku');
+xlabel('Razlika pritisaka')
+ylabel('Broj ispitanika');
 subplot(2, 2, 4);
 D = resultLow;
 E= unique(D),
 F = categorical(D,E,cellstr(num2str(E)));
 histogram(F);
+title('Razlike u donjem pritisku');
+xlabel('Razlika pritisaka')
+ylabel('Broj ispitanika');
 
 
 %h = histogram(resultHigh)
@@ -154,6 +164,9 @@ global firstHigh secondHigh firstLow secondLow age;
 Mean1 = grpstats(firstHigh', age', {'mean'});
 subplot(3,2,3)
 bar( unique(age),Mean1)
+title('Gornji pritisak prvog mjerenja(avg)');
+xlabel('Godine')
+ylabel('Pritisak');
 unAge = unique(age)
 for i=1:size(unique(age))
     text(unAge(i),Mean1(i),num2str(Mean1(i),'%0.2f'),...
@@ -165,6 +178,9 @@ ylim([0 200])
 Mean2 = grpstats(firstLow', age', {'mean'});
 subplot(3,2,4)
 bar( unique(age),Mean2)
+title('Donji pritisak prvog mjerenja(avg)');
+xlabel('Godine')
+ylabel('Pritisak');
 unAge = unique(age)
 for i=1:size(unique(age))
     text(unAge(i),Mean2(i),num2str(Mean2(i),'%0.2f'),...
@@ -176,6 +192,9 @@ ylim([0 200])
 Mean3 = grpstats(secondHigh', age', {'mean'});
 subplot(3,2,5)
 bar( unique(age),Mean3)
+title('Gornji pritisak drugog mjerenja(avg)');
+xlabel('Godine')
+ylabel('Pritisak');
 unAge = unique(age)
 for i=1:size(unique(age))
     text(unAge(i),Mean3(i),num2str(Mean3(i),'%0.2f'),...
@@ -187,6 +206,9 @@ ylim([0 200])
 Mean4 = grpstats(secondLow', age', {'mean'});
 subplot(3,2,6)
 bar( unique(age),Mean4)
+title('Donji pritisak drugog mjerenja(avg)');
+xlabel('Godine')
+ylabel('Pritisak');
 unAge = unique(age)
 for i=1:size(unique(age))
     text(unAge(i),Mean4(i),num2str(Mean4(i),'%0.2f'),...
@@ -210,6 +232,9 @@ global firstHigh secondHigh firstLow secondLow age;
 Mean1 = grpstats((firstHigh-secondHigh)', age', {'mean'});
 subplot(2,2,3)
 bar( unique(age),Mean1)
+title('Razlika prosjecnog gornjeg pritiska po godinama');
+xlabel('Godine')
+ylabel('Razlika pritiska');
 unAge = unique(age)
 for i=1:size(unique(age))
     if Mean1(i) >= 0 
@@ -227,6 +252,9 @@ ylim([-10 10])
 Mean2 = grpstats((firstLow-secondLow)', age', {'mean'});
 subplot(2,2,4)
 bar( unique(age),Mean2)
+title('Razlika prosjecnog donjeg pritiska po godinama');
+xlabel('Godine')
+ylabel('Razlika pritiska');
 unAge = unique(age)
 for i=1:size(unique(age))
     if Mean2(i) >= 0
@@ -250,12 +278,24 @@ function pmt_Callback(hObject, eventdata, handles)
 global firstHigh secondHigh firstLow secondLow age;
 subplot(3,2,3)
 histogram(firstHigh);
+title('Rezultati gornjeg pritiska prvog mjerenja');
+xlabel('Rezultat mjerenja')
+ylabel('Broj ispitanika');
 subplot(3,2,4)
 histogram(secondHigh);
+title('Rezultati gornjeg pritiska drugog mjerenja');
+xlabel('Rezultat mjerenja')
+ylabel('Broj ispitanika');
 subplot(3,2,5)
 histogram(firstLow);
+title('Rezultati donjeg pritiska prvog mjerenja');
+xlabel('Rezultat mjerenja')
+ylabel('Broj ispitanika');
 subplot(3,2,6)
 histogram(secondLow);
+title('Rezultati donjeg pritiska drugog mjerenja');
+xlabel('Rezultat mjerenja')
+ylabel('Broj ispitanika');
 % hObject    handle to pmt (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -267,10 +307,11 @@ global firstHigh secondHigh firstLow secondLow age;
 
 Mean1 = mean(firstHigh)
 Mean2 = mean(secondHigh)
-values = {'Day 1', 'Day 2'}
+values = {'Dan 1', 'Dan 2'}
 MeanArray = [Mean1, Mean2]
 subplot(2,2,3)
 bar(categorical( values), MeanArray)
+title('Prosjecan gornji pritisak po danima');
 for i=1:2
 text(i, MeanArray(i),num2str(MeanArray(i),'%0.2f'),...
                'HorizontalAlignment','center',...
@@ -280,10 +321,11 @@ ylim([0 150])
 
 Mean3 = mean(firstLow)
 Mean4 = mean(secondLow)
-values2 = {'Day 1', 'Day 2'}
+values2 = {'Dan 1', 'Dan 2'}
 MeanArray2 = [Mean3, Mean4]
 subplot(2,2,4)
-bar(categorical( values2), MeanArray2)
+bar(categorical( values2), MeanArray2);
+title('Prosjecan donji pritisak po danima');
 for i=1:2
 text(i, MeanArray2(i),num2str(MeanArray2(i),'%0.2f'),...
                'HorizontalAlignment','center',...
@@ -300,21 +342,33 @@ function pba_Callback(hObject, eventdata, handles)
 global firstHigh secondHigh firstLow secondLow age;
 subplot(3,2,3)
 plot(age, firstHigh, '.');
+title('Rezultati prvog mjerenja gornjeg pritiska po godinama');
+xlabel('Godine')
+ylabel('Rezultat mjerenja');
 xlim([15, 45])
 ylim([70, 160])
 
 subplot(3,2,4)
 plot(age, secondHigh, '.');
+title('Rezultati drugog mjerenja gornjeg pritiska po godinama');
+xlabel('Godine')
+ylabel('Rezultat mjerenja');
 xlim([15, 45])
 ylim([70, 160])
 
 subplot(3,2,5)
 plot(age, firstLow, '.');
+title('Rezultati prvog mjerenja donjeg pritiska po godinama');
+xlabel('Godine')
+ylabel('Rezultat mjerenja');
 xlim([15, 45])
 ylim([40, 110])
 
 subplot(3,2,6)
 plot(age, secondLow, '.');
+title('Rezultati drugog mjerenja donjeg pritiska po godinama');
+xlabel('Godine')
+ylabel('Rezultat mjerenja');
 xlim([15, 45])
 ylim([40, 110])
 % hObject    handle to pba (see GCBO)
